f40b648cd21af000915fc5cc4a37c52ae29a7fe6
NamKungSujeong <nksj98@naver.com>
2023-07-12 23:07:41 +0900

    ~167강

---
+++ section16_js/index.js
@@ -0,0 +1,23 @@
+// boolean
+
+// 1. 비교 연산자
+// > , < , <=, >=
+// 문자열을 비교할 경우 문자의 유니코드 값으로 비교
+
+// == vs ===
+// == : 두 값이 같은지 비교해 주지만 타입 비교는 하지 않음
+// "1" == 1 // true가 나옴
+
+// === : 값 뿐만 아니라 타입까지 비교
+// "1" === 1 // false가 나옴
+
+// != , !== 도 마찬가지
+
+// 2. console.log() : 콘솔에 값을 출력해 볼 수 있게 해주는 메서드
+// console.log("lololo"); // 콘솔에 lololo 출력
+// 3. alert("something") : ""안의 내용으로 알림창을 띄움
+// alert("haha");
+// 4. prompt() : 값을 입력할 수 있는 프롬프트 팝업 창을 띄움
+// let userInput = prompt("enter a number");
+// userInput 값에 유저가 프롬프트에 입력한 값이 문자열로 저장됨
+console.log("connected!");
