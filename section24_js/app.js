aeaff753ac0ee72a1a75e3872275e4485c1340e1
NamKungSujeong <nksj98@naver.com>
2023-07-31 23:39:43 +0900

    DOM

--- section24_js/app.js
+++ section24_js/app.js
@@ -9,3 +9,15 @@
 // id를 이용해 요소를 선택
 
 document.getElementById("banner");
+
+// 2. document.getElementsByTagName
+// 해당 태그 네임 요소를 모두 선택
+
+const allImage = document.getElementsByTagName("img");
+
+for (let img of allImage) {
+  console.log(img);
+}
+
+// 3. document.getElementsByClassName
+// 해당 클래스 요소를 모두 반환
