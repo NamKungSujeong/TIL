11010ad47e146146ee8c03b35931f0a59a445193
NamKungSujeong <nksj98@naver.com>
2023-08-10 21:27:41 +0900

    ajax, api

---
+++ section28_js/app.js
@@ -0,0 +1,10 @@
+// 1. ajax
+// 비동기식 js와 xml
+// 데이터를 꺼낼 수도 어디론가 보낼 수도 있음
+
+// 2. APIs(Application Programming Interface)
+// js로 ajax 요청을 할 때 HTML, CSS , JS가 아닌 순수한 정보를 원하는데
+// 이때 필요한게 api
+// 컴퓨터가 여러 소프트웨어와 상호 작용하거나 소통하는 모든 인터페이스를 의미하는 광범위한 용어
+
+// webAPI : 웹, HTTP를 기반으로 하는 인터페이스
