5593235966bbc324f27d7e78c078f4edda861c75
NamKungSujeong <nksj98@naver.com>
2023-10-27 22:47:15 +0900

    mongoose 모델 만들기

--- section37_mongoose/index.js
+++ section37_mongoose/index.js
@@ -7,3 +7,22 @@ async function main() {
   console.log("CONNECTION OPEN!");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+// 스키마 정의
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
+
+// 스키마 모델 만들기
+const Movie = mongoose.model("Movie", movieSchema);
+
+// DB 생성
+const amadeus = new Movie({
+  title: "Amadeus",
+  year: 1986,
+  score: 9.2,
+  rating: "R",
+});
