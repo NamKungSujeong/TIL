10a7532369fb99a283db369e50a77f5cd426aa89
NamKungSujeong <nksj98@naver.com>
2023-11-09 22:46:20 +0900

    mongoose, express연결

--- section38/index.js
+++ section38/index.js
@@ -15,7 +15,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/dog", (req, res) => {
-  res.send("WOOF");
+  res.send("WOOF!!");
 });
 
 app.listen(3000, () => {
