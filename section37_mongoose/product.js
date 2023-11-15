b361439d77ed4384da235e2de114379d6e15b798
NamKungSujeong <nksj98@naver.com>
2023-11-15 12:11:05 +0900

    수정

--- section37_mongoose/product.js
+++ section37_mongoose/product.js
@@ -37,33 +37,38 @@ const productSchema = new mongoose.Schema({
   },
 });
 
-const Product = mongoose.model("Product", productSchema);
-
 const bike = new Product({ name: "Bike", price: 999 });
 
 // save 바로 실행 가능
-// bike
-//   .save()
-//   .then((data) => console.log(data))
-//   .catch((err) => console.log("error"));
+bike
+  .save()
+  .then((data) => console.log(data))
+  .catch((err) => console.log("error"));
 
-Product.findOneAndUpdate(
-  { name: "Bike" },
-  { price: -10 },
-  { new: true, runValidators: true }
-).then((data) => console.log(data).catch((err) => console.log(err)));
+// Product.findOneAndUpdate(
+//   { name: "Bike" },
+//   { price: -10 },
+//   { new: true, runValidators: true }
+// ).then((data) => console.log(data).catch((err) => console.log(err)));
 // price에 음수값이 들어갔는데도 작동을 함
 // 뭔가를 만들면 유효성 검사가 자동으로 적용되는데 그게 업데이트되고 나서는
 // 안되기 때문에 Mongoose한테 계속 유효성 검사를 적용하라고 얘기해줘야 함
 // runValidators 옵션을 true로 넣어주기
 
-// productSchema.method.greet = function () {
-//   console.log("Hello");
-// };
+productSchema.methods.greet = function () {
+  console.log("HELLLO!!! HI!! HOWDY!!! ");
+  console.log(`- from ${this.name}`);
+};
 
 // 인스턴스 메서드
-productSchema.method.toggleOnSale = function () {
+productSchema.methods.toggleOnSale = function () {
   this.onSale = !this.onSale;
+  // 저장 시키기
+  return this.save();
+};
+
+productSchema.methods.addCategory = function () {
+  this.categories.push("outdoor");
   return this.save();
 };
 
@@ -71,15 +76,8 @@ productSchema.method.toggleOnSale = function () {
 productSchema.statics.fireSale = function () {
   return this.updateMany({}, { onSale: true, price: 0 });
 };
+// 여기에서는 this가 모델을 의미
 
-const findProduct = async () => {
-  const foundProduct = await Product.findOne({ name: "Bike" });
-  // findProduct.greet();
-  console.log(foundProduct);
-  await foundProduct.toggleOnSale();
-  console.log(foundProduct);
-};
+const Product = mongoose.model("Product", productSchema);
 
-// findProduct();
-
-Product.fireSale().then((res) => console.log(res));
+// Product.fireSale().then((res) => console.log(res));
