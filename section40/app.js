0d4b88b925d17a32fefdc7be02ab2887ae216a7e
NamKungSujeong <nksj98@naver.com>
2023-12-12 19:21:37 +0900

    미들웨어 패스워드 설정 데모

--- section40/app.js
+++ section40/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 
-
 app.use((req, res, next) => {
   req.requestTime = Date.now();
   console.log(req.method, req.path);
   next();
-
-
 });
 
+// 미들웨어 패스워드 설정 데모
+app.use((req, res, next) => {
+  const { password } = req.query;
+  if (password === "ffffff") {
+    next();
+  }
+  res.send("YOU NEED A PASSWORD");
+});
+
+app.get("/secret", (req, res) => {
+  res.send("SECRET");
+});
+
+app.listen(3000, () => {
+  console.log("port is listening");
+});
